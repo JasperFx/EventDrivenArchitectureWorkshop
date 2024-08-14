2730f93062e4e429360ad48dabc97826ff21639a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-08-14 06:39:56 -0500

    All tests are passing for 003, 004, and 005

---
+++ 005_Wolverine/IncidentService/Exceptions.cs
@@ -0,0 +1,4 @@
+
+// These are faked up just to make a point
+public class MakeBelieveSubsystemIsDownException : Exception{}
+public class InvalidInputThatCouldNeverBeProcessedException : Exception{}
--- 005_Wolverine/IncidentService/Program.cs
+++ 005_Wolverine/IncidentService/Program.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using IncidentService;
+using JasperFx.Core;
 using Marten;
 using Marten.Events.Projections;
 using Oakton;
@@ -55,6 +56,34 @@ builder.Services.AddMarten(opts =>
 builder.Host.UseWolverine(opts =>
 {
     opts.Policies.AutoApplyTransactions();
+    
+    // Applies a transactional inbox/outbox on 
+    // local queues
+    opts.Policies.UseDurableLocalQueues();
+    
+    opts.LocalQueueFor<TryAssignPriority>()
+        // By default, local queues allow for parallel processing with a maximum
+        // parallel count equal to the number of processors on the executing
+        // machine, but you can override the queue to be sequential and single file
+        .Sequential()
+
+        // Or add more to the maximum parallel count!
+        .MaximumParallelMessages(10)
+
+        // Pause processing on this local queue for 1 minute if there's
+        // more than 20% failures for a period of 2 minutes
+        .CircuitBreaker(cb =>
+        {
+            cb.PauseTime = 1.Minutes();
+            cb.SamplingPeriod = 2.Minutes();
+            cb.FailurePercentageThreshold = 20;
+            
+            // Definitely worry about this type of exception
+            cb.Include<TimeoutException>();
+            
+            // Don't worry about this type of exception
+            cb.Exclude<InvalidInputThatCouldNeverBeProcessedException>();
+        });
 });
 
 // Add services to the container.
